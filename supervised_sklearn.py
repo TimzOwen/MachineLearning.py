e4c8cbf98b0d2e06107dc5dd673dbea15d6c3ee4
Timz Owen <ktimothy@kabarak.ac.ke>
2020-05-20 17:27:34 +0300

    Update supervised_sklearn.py
    model performance

--- supervised_sklearn.py
+++ supervised_sklearn.py
@@ -103,7 +103,146 @@ print("Prediction: {}".format(new_prediction))
 
 
 #UPNEXT- MEASURING MODEL PERFORMANCE
-#split your data into training and testing 
+#split your data into training and testing
+
+#train/test split
+from sklearn.model_selection import train_test_split
+
+X_train,x_test,y_train,y_test = train_test_split(X, y, test_size=0.3, random_state=21, stratify=y)
+
+#instantiate the classifier
+knn = KNeighborsClassifier(n_neighbors=8)
+
+#feed in the test data
+knn.fit(X_train, y_train)
+
+#predict
+y_pred = knn.predict(x_test)
+
+print("test set prediction:\n {}".format(y_pred))
+
+#check accuracy test in percentage
+knn.score(x_test, y_test)   #0.95555555 output sample meaning 95% accurate
+
+# Import necessary modules
+from sklearn import datasets
+import matplotlib.pyplot as plt
+
+# Load the digits dataset: digits
+digits = datasets.load_digits()
+
+# Print the keys and DESCR of the dataset
+print(digits.keys())
+print(digits.DESCR)
+
+# Print the shape of the images and data keys
+print(digits.images.shape)
+print(digits.data.shape)
+
+# Display digit 1010
+plt.imshow(digits.images[1010], cmap=plt.cm.gray_r, interpolation='nearest')
+plt.show()
+
+#PERform MNIST no. detection
+# Import necessary modules
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+
+# Create feature and target arrays
+X = digits.data
+y = digits.target
+
+# Split into training and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=42, stratify=y)
+
+# Create a k-NN classifier with 7 neighbors: knn
+knn = KNeighborsClassifier(n_neighbors=7)
+
+# Fit the classifier to the training data
+knn.fit(X_train, y_train)
+
+# Print the accuracy
+print(knn.score(X_test, y_test))
+
+# Setup arrays to store train and test accuracies
+neighbors = np.arange(1, 9)
+train_accuracy = np.empty(len(neighbors))
+test_accuracy = np.empty(len(neighbors))
+
+# Loop over different values of k
+for i, k in enumerate(neighbors):
+    # Setup a k-NN Classifier with k neighbors: knn
+    knn = KNeighborsClassifier(n_neighbors=k)
+
+    # Fit the classifier to the training data
+    knn.fit(X_train,y_train)
+    
+    #Compute accuracy on the training set
+    train_accuracy[i] = knn.score(X_train, y_train)
+
+    #Compute accuracy on the testing set
+    test_accuracy[i] = knn.score(X_test, y_test)
+
+# Generate plot
+plt.title('k-NN: Varying Number of Neighbors')
+plt.plot(neighbors, test_accuracy, label = 'Testing Accuracy')
+plt.plot(neighbors, train_accuracy, label = 'Training Accuracy')
+plt.legend()
+plt.xlabel('Number of Neighbors')
+plt.ylabel('Accuracy')
+plt.show()
+
+
+#REGRESSION WITH BOSTON HOUSE DATASET
+
+#first import and load dataset
+boston = pd.load_csv('boston.csv')
+
+#display the recent data
+print(boston.head())
+
+#now create feature and Target
+X = boston.drop('MEDV',axis=1).Values
+y = boston['MEDV'].values
+
+#predict house from a single feature column no. 5
+X_room = X[:,5]
+
+#you can check the type of the data
+type(X_rooms), type(y) #numpy arrays
+
+#reshape
+y = y.reshape(-1 ,1)
+X_rooms = X_rooms.reshape(-1, 1)
+
+#plot house numbers vs number of rooms
+plt.scatter(X_rooms, y)
+plt.ylabel('value of houses/1000($)')
+plt.xlabel('No. of rooms')
+plt.show();
+
+#Fitting a Linear Regression Model
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+#instantiate the model
+reg = LinearRegression()
+
+#fit the model
+reg.fit(X_rooms, y)
+
+#prediction_space
+prediction_space =  np.linspace(min(X_rooms), max(X_rooms)).reshape(-1,1)
+
+#plot the line
+plt.scatter(X_rooms, y, color='blue')
+plt.plot(prediction_space, reg.predict(prediction_space), color='black', linewidth=3)
+plt.show()
+
+
+#Importing data for supervised Learning to predict Life expectancy of a country using their GDP
+
+
 
 
 
