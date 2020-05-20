f7830b1df520f7f06c0f00e803d260aaba9f30f9
Timz Owen <ktimothy@kabarak.ac.ke>
2020-05-20 15:11:37 +0300

    Update supervised_sklearn.py
    training recap code

--- supervised_sklearn.py
+++ supervised_sklearn.py
@@ -64,3 +64,46 @@ x_new.shape
 print('prediction: {}'.format(prediction))
 
 [1,1,0] #output prediction
+
+
+#Prediction002 
+# Import KNeighborsClassifier from sklearn.neighbors
+from sklearn.neighbors import KNeighborsClassifier
+
+# Create arrays for the features and the response variable
+y = df['party'].values
+X = df.drop('party', axis=1).values
+
+# Create a k-NN classifier with 6 neighbors
+knn = KNeighborsClassifier(n_neighbors=6)
+
+# Fit the classifier to the data
+knn.fit(X,y)
+
+# Import KNeighborsClassifier from sklearn.neighbors
+from sklearn.neighbors import KNeighborsClassifier 
+
+# Create arrays for the features and the response variable
+y = df['party'].values
+X = df.drop('party',axis=1).values
+
+# Create a k-NN classifier with 6 neighbors: knn
+knn = KNeighborsClassifier(n_neighbors=6)
+
+# Fit the classifier to the data
+knn.fit(X,y)
+
+# Predict the labels for the training data X
+y_pred = knn.predict(X)
+X_new = knn.predict(X_new)
+
+# Predict and print the label for the new data point X_new
+new_prediction = np.array(X_new)
+print("Prediction: {}".format(new_prediction))
+
+
+#UPNEXT- MEASURING MODEL PERFORMANCE
+#split your data into training and testing 
+
+
+
