60174a3d98c2fd3d84f053c94a469fff46b07f78
Timz Owen <ktimothy@kabarak.ac.ke>
2020-05-21 19:30:45 +0300

    Update supervised_sklearn.py
    Hyper parameter tunning

--- supervised_sklearn.py
+++ supervised_sklearn.py
@@ -660,3 +660,46 @@ cv_scores = cross_val_score(logreg, X,y, cv=5, scoring='roc_auc')
 print(cv_scores)
 
 
+#HYPER PARAMETER TUNNING 
+#using Grid search score
+from sklearn.model_selection import GridSearchCV
+
+param_grid = {'n_neighbors': np.arange(1, 50)}
+
+knn = KNeighborsClassifier()
+
+knn_cv = GridSearchCV(knn, param_grid, cv=5)
+
+knn_cv.fit(X,y)
+knn_cv.best_params_
+knn_cv.best_score_
+
+
+#Decision tree
+# Import necessary modules
+from scipy.stats import randint
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import RandomizedSearchCV
+
+# Setup the parameters and distributions to sample from: param_dist
+param_dist = {"max_depth": [3, None],
+              "max_features": randint(1, 9),
+              "min_samples_leaf": randint(1, 9),
+              "criterion": ["gini", "entropy"]}
+
+# Instantiate a Decision Tree classifier: tree
+tree = DecisionTreeClassifier()
+
+# Instantiate the RandomizedSearchCV object: tree_cv
+tree_cv = RandomizedSearchCV(tree, param_dist, cv=5)
+
+# Fit it to the data
+tree_cv.fit(X,y)
+
+# Print the tuned parameters and score
+print("Tuned Decision Tree Parameters: {}".format(tree_cv.best_params_))
+print("Best score is {}".format(tree_cv.best_score_))
+
+
+
+
